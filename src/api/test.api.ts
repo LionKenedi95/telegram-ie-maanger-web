342a272a365319018c7585ebe109f1c60ac42bb3
Vadim Demchenko <v.demchenko@revizto.com>
2024-11-24 13:43:51 +0400

    test api 1

--- src/api/test.api.ts
+++ src/api/test.api.ts
@@ -1,9 +1,15 @@
 import API_URL from "@/constants/ApiUrl";
 
-export const tryApi = () => {
+export const tryApi = (initData: string) => {
   console.log('tryApi')
   return fetch(API_URL, {
     method: "POST",
+    headers: {
+      'Content-Type': 'application/json', // Тип данных, которые мы отправляем
+    },
+    body: JSON.stringify({
+      initData,
+    }),
   })
     .then(result => {
       console.log('result', result)
